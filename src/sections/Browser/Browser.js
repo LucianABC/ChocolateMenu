7429e8ad94f80b843c1a21113bbaa141f6e4dfe2
LucianABC <louabecasis@gmail.com>
2020-02-17 13:00:40 -0300

    asdas

--- src/sections/Browser/Browser.js
+++ src/sections/Browser/Browser.js
@@ -3,6 +3,7 @@ import {BrowserRouter,
         Link,
         Switch,
         Route} from 'react-router-dom';
+import './Browser.scss';
 import ProductsAndActions from '../Products+Actions/ProductsAndActions'
 import BrandsSection from '../Brands/BrandsSection';
 import Params from '../Params/Params';
@@ -26,31 +27,32 @@ const Browser = () => {
     ];
     
     return (
-        <BrowserRouter>
-            {            
-                Sections.map(section=>{
-                    if (section.name!=="Params"){
-                    return (<Link to= {section.route}>{section.name}</Link>)
-                    }
-                })
-         
-            }    
-                <Link to="/params/8"> 8 </Link>     
-                <Link to="/params/7"> 7 </Link>
-                <Link to="/params/6"> 6 </Link>
-                <Link to="/params/5"> 5 </Link>
-                <Link to="/params/4"> 4 </Link>
-            <Switch>
-                {
+        <div className="browser">
+            <BrowserRouter>
+                <div className="browser-menu">
+                    
+                    {            
                     Sections.map(section=>{
-                        return (<Route path= {section.route} 
-                                component={section.section}></Route>)
-                    })
-                }
-            </Switch>
-        
-       
-        </BrowserRouter>
+                        if (section.name!=="Params"){
+                        return (<Link className="menu-item" to= {section.route}>{section.name}</Link>)
+                        }
+                    })}    
+                    <Link className="menu-item" to="/params/8"> 8 </Link>     
+                    <Link className="menu-item"to="/params/7"> 7 </Link>
+                    <Link className="menu-item" to="/params/6"> 6 </Link>
+                    <Link className="menu-item" to="/params/5"> 5 </Link>
+                    <Link className="menu-item"to="/params/4"> 4 </Link>
+                </div>
+                <Switch>
+                    {
+                        Sections.map(section=>{
+                            return (<Route path= {section.route} 
+                                    component={section.section}></Route>)
+                        })
+                    }
+                </Switch>       
+            </BrowserRouter>
+        </div>
     )
 }
 
