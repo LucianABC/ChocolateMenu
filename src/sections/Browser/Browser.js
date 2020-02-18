92b791e916f4aa9c64390ee6ff1f214d3f7b6f03
LucianABC <louabecasis@gmail.com>
2020-02-18 10:56:41 -0300

    useReducer

--- src/sections/Browser/Browser.js
+++ src/sections/Browser/Browser.js
@@ -22,7 +22,7 @@ const Browser = () => {
         },
         {   section: Params,
             name:"Params",
-            route: "/params/:id"
+            route: "/params"
         }
     ];
     
@@ -30,18 +30,10 @@ const Browser = () => {
         <div className="browser">
             <BrowserRouter>
                 <div className="browser-menu">
-                    
                     {            
                     Sections.map(section=>{
-                        if (section.name!=="Params"){
                         return (<Link className="menu-item" to= {section.route}>{section.name}</Link>)
-                        }
                     })}    
-                    <Link className="menu-item" to="/params/8"> 8 </Link>     
-                    <Link className="menu-item"to="/params/7"> 7 </Link>
-                    <Link className="menu-item" to="/params/6"> 6 </Link>
-                    <Link className="menu-item" to="/params/5"> 5 </Link>
-                    <Link className="menu-item"to="/params/4"> 4 </Link>
                 </div>
                 <Switch>
                     {
--- src/sections/Params/Params.js
+++ src/sections/Params/Params.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import {useParams} from 'react-router-dom';
+import React, {useReducer, useState} from 'react';
+import Title from '../../components/Title'
 
+
+const reducer = (state, action)=>{
+    switch(action.type){
+        case "changeUser":
+            return {...state, user: action.payload };
+        case "changeName":
+            return {...state, name: action.payload};
+        case "changeMail":
+            return {...state, mail: action.payload};
+        default:
+            return state;
+    }
+};
 const Params = () => {
-    const {id} = useParams();
+    const [name, setName] = useState("");
+    const [mail, setMail] = useState("");
+    const [userName, setUsername] = useState("");
+    const [state, dispatch] = useReducer(reducer, {user: userName, mail: mail, name: name});
+    const changeUser=()=> dispatch({type: "changeUser", payload: userName});
+    const changeMail=()=> dispatch({type: "changeMail", payload: mail});
+    const changeName=()=> dispatch({type:"changeName", payload: name});
+
     return(
         <div>
-            <h1>El id es {id}</h1>
-            
-        </div>       
-    )
-}
+            <Title>Esto es un titulo</Title>
+            <div>
+                <input type="text" onChange = { e=> setName(e.target.value)}/>
+                <button onClick={changeName}>Send name</button>
 
+                <input type="text" onChange = { e=> {setUsername(e.target.value)}}/>
+                <button onClick={changeUser}>Send username</button>
+                
+                <input type="email" onChange = { e=> {setMail(e.target.value)}}/>
+                <button onClick={changeMail}> Send mail</button>
+            </div>
+            
+            <span> Hola {state.name}, tu usuario es {state.user} mail es {state.mail}</span>
+        </div>       
+    );
+}
 
 export default Params
